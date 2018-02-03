572bba79ccd3fb4dd02fa798cf2898250244a6e0
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2018-02-03 14:50:28 -0600

    updating set-up

--- keys.js
+++ keys.js
@@ -1,15 +1,13 @@
-```js
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: BU4NhW1l64Gva5RSXKZI2ZuPj,
-  consumer_secret: wOd6C4d8ixDhS03KtbcSTqTbsyA4Aq8hCLAovAjmq4hKNNcPli,
-  access_token_key: 1517100054-Ja2G0XsFYFo58GQ03VFSQncrHi6qr5ns5G4aKIM,
-  access_token_secret: x6X4C41vLuq1TO52GhnIIvdkH9tyj8AVtHUv8txF8j1Re
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.3f878e381e46479783adb4b883910545,
-  secret: process.env.f7ae58fbe4344712a3ce94110feea449
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
-```
--- liri.js
+++ liri.js
@@ -1,7 +1,3 @@
-```js
-require("dotenv").config();
-```
-
 // global variables
 var fs = require("fs");
 var keys = require("./keys.js");
@@ -9,9 +5,56 @@ var userCommand = process.argv[2];
 var userInput = process.argv[3];
 
 var twitter = require("twitter");
-var spotify = require("spotify");
+var spotify = require("node-spotify-api");
 var request = require("request");
 // var computerSearch = [];
 // ^^^ not sure what this is
 
-// function to console movies
+// "movie-this '<movie name here>'" function
+function consoleMovies (body) {
+
+    // title, year movie came out, IMDB rating, RT rating, country of production, language, plot, actors
+
+    console.log(`Title: ${JSON.parse(body).Title}`);
+    console.log(`Year: ${JSON.parse(body).Year}`);
+    console.log(`IMDB Rating: ${JSON.parse(body).imdbRating}`);
+    console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`); 
+    console.log(`Country of Production: ${JSON.parse(body).Country}`);
+    console.log(`Language: ${JSON.parse(body).Language}`);
+    console.log(`Plot: ${JSON.parse(body).Plot}`);
+    console.log(`Actors: ${JSON.parse(body).Actors}`);
+
+}
+
+// "spotify-this-song"
+
+
+// IMDB API call 
+
+// consider using a switch
+if (userCommand === "movie-this" && userInput) {
+
+    var queryURL = `http://www.omdbapi.com/?t="${userInput}"&y=&plot=short&apikey=trilogy`
+
+    request(queryURL, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            consoleMovies(body);
+        }
+
+    });
+
+} else if (userCommand === "movie-this" && "undefined") { 
+
+    userInput = "Mr. Nobody";
+
+    queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+
+    request(queryURL, function (error, response, body){
+
+        if (!error && response.statusCode === 200) {
+           consoleMovies(body);
+        }
+    });
+
+}
