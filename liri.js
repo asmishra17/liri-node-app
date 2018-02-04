6da0baccc7525168754db2ed437f42bdade8cfc9
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2018-02-03 19:36:42 -0600

    making function to call twitter API

--- liri.js
+++ liri.js
@@ -1,19 +1,21 @@
 // global variables
 var fs = require("fs");
 var keys = require("./keys.js");
-var userCommand = process.argv[2];
-var userInput = process.argv[3];
 
-var twitter = require("twitter");
-var spotify = require("node-spotify-api");
 var request = require("request");
-// var computerSearch = [];
-// ^^^ not sure what this is
+
+var spotify = require("node-spotify-api");
+
+// store all of the arguments in an array
+var nodeArgs = process.argv;
+var userRequest = process.argv[2];
+
+// create an empty variable for holding the movie name
+var movieName = "";
 
 // "movie-this '<movie name here>'" function
-function consoleMovies (body) {
 
-    // title, year movie came out, IMDB rating, RT rating, country of production, language, plot, actors
+function consoleMovies (body) {
 
     console.log(`Title: ${JSON.parse(body).Title}`);
     console.log(`Year: ${JSON.parse(body).Year}`);
@@ -26,29 +28,32 @@ function consoleMovies (body) {
 
 }
 
-// "spotify-this-song"
+// loop through all words in the node argument 
+for (var i = 3; i < nodeArgs.length; i++) {
 
+    if (i > 3 && i < nodeArgs.length) {
+        movieName = movieName + "+" + nodeArgs[i];
+    } else {
+        movieName += nodeArgs[i];
+    }
+}
 
 // IMDB API call 
 
-// consider using a switch
-if (userCommand === "movie-this" && userInput) {
+var queryURL = `http://www.omdbapi.com/?t="${movieName}"&y=&plot=short&apikey=trilogy`
 
-    var queryURL = `http://www.omdbapi.com/?t="${userInput}"&y=&plot=short&apikey=trilogy`
+if (userRequest === "movie-this" && movieName) {
 
     request(queryURL, function (error, response, body) {
-
         if (!error && response.statusCode === 200) {
             consoleMovies(body);
         }
-
     });
 
-} else if (userCommand === "movie-this" && "undefined") { 
+} else if (userRequest === "movie-this" && "undefined") { 
 
-    userInput = "Mr. Nobody";
-
-    queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+    queryURL = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+    
 
     request(queryURL, function (error, response, body){
 
@@ -56,5 +61,66 @@ if (userCommand === "movie-this" && userInput) {
            consoleMovies(body);
         }
     });
-
 }
+
+/// Twitter API call
+var Twitter = require("twitter");
+
+var client = new Twitter({
+    consumer_key: "BU4NhW1l64Gva5RSXKZI2ZuPj",
+    consumer_secret: "wOd6C4d8ixDhS03KtbcSTqTbsyA4Aq8hCLAovAjmq4hKNNcPli",
+    access_token_key: "1517100054-Ja2G0XsFYFo58GQ03VFSQncrHi6qr5ns5G4aKIM",
+    access_token_secret: "x6X4C41vLuq1TO52GhnIIvdkH9tyj8AVtHUv8txF8j1Re"
+});
+
+// "my-tweets" function
+var params = {screen_name: 'asmishra93'};
+
+function twitterFunction () {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("*-*-*-Your Tweets-*-*-*");
+
+            console.log(`Tweet Created On: ${tweets[0].created_at}`);
+            console.log(`Tweet: ${tweets[0].text}`)
+
+            console.log(`Tweet Created On: ${tweets[1].created_at}`);
+            console.log(`Tweet: ${tweets[1].text}`)
+
+            console.log(`Tweet Created On: ${tweets[2].created_at}`);
+            console.log(`Tweet: ${tweets[2].text}`)
+
+            console.log(`Tweet Created On: ${tweets[3].created_at}`);
+            console.log(`Tweet: ${tweets[3].text}`)
+
+            console.log(`Tweet Created On: ${tweets[4].created_at}`);
+            console.log(`Tweet: ${tweets[4].text}`)
+
+            console.log(`Tweet Created On: ${tweets[5].created_at}`);
+            console.log(`Tweet: ${tweets[5].text}`)
+
+            console.log(`Tweet Created On: ${tweets[6].created_at}`);
+            console.log(`Tweet: ${tweets[6].text}`)
+
+            console.log(`Tweet Created On: ${tweets[7].created_at}`);
+            console.log(`Tweet: ${tweets[7].text}`)
+
+            console.log(`Tweet Created On: ${tweets[8].created_at}`);
+            console.log(`Tweet: ${tweets[8].text}`)
+
+            console.log(`Tweet Created On: ${tweets[9].created_at}`);
+            console.log(`Tweet: ${tweets[9].text}`)
+
+            console.log(`Tweet Created On: ${tweets[10].created_at}`);
+            console.log(`Tweet: ${tweets[10].text}`)
+        } 
+    });
+}
+
+if (userRequest === "my-tweets") {
+    twitterFunction();
+};
+
+// Spotify API call
+
+
